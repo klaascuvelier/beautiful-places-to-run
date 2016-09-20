85c3772aa26d349b0f0a85c44ffa8da8a8c6c6ab
Klaas Cuvelier <cuvelierklaas@gmail.com>
2016-09-19 21:05:22 -0700

    add runs service

---
+++ src/app/services/runs.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { AngularFire, FirebaseListObservable } from "angularfire2";
+import { Run } from '../types/run.type';
+
+@Injectable()
+export class RunsService {
+
+    public runs$:FirebaseListObservable<Array<Run>> = this.af.database.list('runs');
+
+    constructor(private af: AngularFire) {
+
+    }
+
+}
