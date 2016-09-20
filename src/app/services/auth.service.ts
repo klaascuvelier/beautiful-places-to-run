fd850e706234e7da11a8070cd36e34373f74dcf0
Klaas Cuvelier <cuvelierklaas@gmail.com>
2016-09-20 14:25:50 -0700

    Make auth a replay subject with Auth type

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,13 +1,29 @@
 import { Injectable } from '@angular/core';
-import { AngularFire, AngularFireAuth, AuthProviders, AuthMethods } from "angularfire2";
+import { AngularFire, AuthProviders, AuthMethods } from "angularfire2";
+import { ReplaySubject } from 'rxjs';
+import { Auth } from '../types/auth.type';
 
 @Injectable()
 export class AuthService {
 
-    public auth$: AngularFireAuth = this.af.auth;
-
+    public auth$: ReplaySubject<Auth> = new ReplaySubject<Auth>(null);
 
     constructor(private af: AngularFire) {
+        this.af.auth
+            .subscribe(auth => {
+                let user:Auth = null;
+
+                if (auth !== null) {
+                    user = {
+                        displayName: auth.auth['displayName'],
+                        emailAddress: auth.auth['emailAddress'],
+                        uid: auth.uid,
+                        isAdmin: auth.uid === 'JjWJHzU35XesS0RL1LCxkHTBS8h2'
+                    };
+                }
+
+                this.auth$.next(user);
+            });
     }
 
     doLogin () {
