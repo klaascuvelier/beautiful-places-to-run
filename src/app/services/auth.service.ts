10ac2f29d00106edba5ac11a1a0db140dbb3690f
Klaas Cuvelier <cuvelierklaas@gmail.com>
2016-10-19 20:54:29 -0700

    store user info on login

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -2,13 +2,14 @@ import { Injectable } from '@angular/core';
 import { AngularFire, AuthProviders, AuthMethods } from "angularfire2";
 import { ReplaySubject } from 'rxjs';
 import { Auth } from '../types/auth.type';
+import { UsersService } from "./users.services";
 
 @Injectable()
 export class AuthService {
 
     public auth$: ReplaySubject<Auth> = new ReplaySubject<Auth>(null);
 
-    constructor(private af: AngularFire) {
+    constructor(private af: AngularFire, private usersService:UsersService) {
         this.af.auth
             .subscribe(auth => {
                 let user:Auth = null;
@@ -17,12 +18,15 @@ export class AuthService {
                     user = {
                         displayName: auth.auth['displayName'],
                         emailAddress: auth.auth['email'],
+                        avatar: auth.auth['photoURL'],
                         uid: auth.uid,
                         isAdmin: auth.uid === 'JjWJHzU35XesS0RL1LCxkHTBS8h2'
                     };
                 }
 
                 this.auth$.next(user);
+
+                this.usersService.storeUser(user);
             });
     }
 
