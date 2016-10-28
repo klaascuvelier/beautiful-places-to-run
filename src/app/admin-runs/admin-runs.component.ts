1d8587523b119ead4d7279e1b8078fabf97ee15a
Klaas Cuvelier <cuvelierklaas@gmail.com>
2016-10-27 20:26:28 -0700

    add slug on new/update

--- src/app/admin-runs/admin-runs.component.ts
+++ src/app/admin-runs/admin-runs.component.ts
@@ -22,7 +22,22 @@ export class AdminRunsComponent implements OnInit {
         const data = {};
         data[key] = value;
 
+        if ((!item.hasOwnProperty('slug') && item.hasOwnProperty('location'))) {
+            data.slug = getSlug(item.location);
+        }
+        else if (key === 'location') {
+            data.slug = getSlug(value);
+        }
+
         this.runs$.update(item.$key, data);
+
+        function getSlug(location:String):String {
+            return location
+                .trim()
+                .replace(/[^\w\s]/, '')
+                .replace(/\s/g, '-')
+                .toLocaleLowerCase();
+        }
     }
 
     addRun () {
