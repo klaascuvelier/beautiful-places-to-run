63a98e1458f1abfab11c73ce87d1bc04bb7501dd
Klaas Cuvelier <cuvelierklaas@gmail.com>
2016-10-27 20:18:38 -0700

    updates for run details

--- src/app/places/places.component.ts
+++ src/app/places/places.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnDestroy } from '@angular/core';
 import { RunsService } from "../services/runs.service";
 import { AuthService } from "../services/auth.service";
-import { ActivatedRoute } from "@angular/router";
+import { ActivatedRoute, Router } from "@angular/router";
 import { zip } from "rxjs/observable/zip";
 import { Observable, Subscription, Subject } from "rxjs";
 import { UsersService } from "../services/users.services";
 import { Run } from "../types/run.type";
+import { combineLatest } from "rxjs/observable/combineLatest";
 
 const emailRegexp:RegExp = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
 
@@ -22,13 +23,14 @@ export class PlacesComponent implements OnDestroy {
     private selectedRun:Run = null;
 
     private ready:Boolean = false;
-    private subscription:Subscription = null;
+    private subscriptions:Array<Subscription> = [];
 
     constructor(
         private activatedRoute: ActivatedRoute,
         private authService: AuthService,
         private runsService: RunsService,
-        private usersService: UsersService
+        private usersService: UsersService,
+        private router: Router
     ) {
         // Get email from url params
         const urlEmail$ = this.activatedRoute.params.map(params => params['emailAddress'] || null);
@@ -45,11 +47,27 @@ export class PlacesComponent implements OnDestroy {
         // Let data come together
         const data = zip(this.runsService.runs$, runnerData$);
 
-        this.subscription = data.subscribe(([runs, runner]) => {
+        // Wait for runs + runner data
+        const dataSubscription = data.subscribe(([runs, runner]) => {
             this.runs = runs;
             this.runner = runner;
             this.ready = true;
         });
+
+        // Wait for route data and runs
+        const routeSubscription = combineLatest(this.runsService.runs$, this.activatedRoute.params)
+            .subscribe(([runs, params]) => {
+                if (params.hasOwnProperty('location')) {
+                    const run = runs.filter(run => run.slug === params['location'])[0] || null;
+
+                    if (run !== null) {
+                        this.showRunDetails(run);
+                    }
+                }
+            });
+
+        this.subscriptions.push(dataSubscription);
+        this.subscriptions.push(routeSubscription);
     }
 
     getRunnerData (email):Observable<any> {
@@ -67,9 +85,16 @@ export class PlacesComponent implements OnDestroy {
 
     resetRunDetail () {
         this.showRunDetails(null);
+
+        const route = this.activatedRoute.snapshot;
+
+        if (route.params.hasOwnProperty('location')) {
+            const newRoute = route.url.slice(0, -1).map(segment => segment.path).join('/');
+            this.router.navigateByUrl(`/${newRoute}`);
+        }
     }
 
     ngOnDestroy () {
-        this.subscription.unsubscribe();
+        this.subscriptions.forEach(subscription => subscription.unsubscribe());
     }
 }
