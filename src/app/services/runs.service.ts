b44b24f7ae018f5be85c83548b6e72fa333f2d84
Klaas Cuvelier <cuvelierklaas@gmail.com>
2016-10-27 20:25:45 -0700

    Should stay list observable

--- src/app/services/runs.service.ts
+++ src/app/services/runs.service.ts
@@ -1,21 +1,11 @@
 import { Injectable } from '@angular/core';
-import { AngularFire } from "angularfire2";
+import { AngularFire, FirebaseListObservable } from "angularfire2";
 import { Run } from '../types/run.type';
-import { Observable } from "rxjs";
 
 @Injectable()
 export class RunsService {
 
-    public runs$:Observable<Array<Run>> = this.af.database
-        .list('runs')
-        .map(runs => runs.map(run => {
-            run.slug = run.location
-                .trim()
-                .replace(/[^\w\s]/, '')
-                .replace(/\s/g, '-')
-                .toLocaleLowerCase();
-            return run;
-        }));
+    public runs$:FirebaseListObservable<Array<Run>> = this.af.database.list('runs');
 
     constructor(private af: AngularFire) {}
 
