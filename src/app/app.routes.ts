a393c69fb12407df28ea5e06418c77c68a69a7a4
Klaas Cuvelier <cuvelierklaas@gmail.com>
2016-10-28 17:24:40 -0700

    #4 update routes to work with children

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,25 @@ import { Routes } from "@angular/router";
 import { AdminRunsComponent } from "./admin-runs/admin-runs.component";
 import { LandingComponent } from "./landing/landing.component";
 import { PlacesComponent } from "./places/places.component";
+import { RunDetailComponent } from "./run-detail/run-detail.component";
 
 export const appRoutes: Routes = [
     { path: '', component: LandingComponent },
     { path: 'admin/runs', component: AdminRunsComponent },
-    { path: 'places', component: PlacesComponent },
-    { path: 'places/:location', component: PlacesComponent },
-    { path: 'runner/:emailAddress', component: PlacesComponent },
-    { path: 'runner/:emailAddress/:location', component: PlacesComponent },
+    {
+        path: 'places',
+        component: PlacesComponent,
+        children: [
+            { path: '' },
+            { path: ':location', component: RunDetailComponent }
+        ]
+    },
+    {
+        path: 'runner/:emailAddress',
+        component: PlacesComponent,
+        children: [
+            { path: '' },
+            { path: ':location', component: RunDetailComponent }
+        ]
+    }
 ];
